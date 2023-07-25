93967fbc3df5a892dc5d0d51596918fbc38331ed
jjjjjeongwon <jjjjjwon21@gmail.com>
2023-07-26 01:14:27 +0900

    :white_check_mark: Feat: 채팅 말풍선이 n초가 지나면 사라지게 구현

--- src/components/ui/Three/chat/TalkBubble.js
+++ src/components/ui/Three/chat/TalkBubble.js
@@ -1,12 +1,37 @@
-import { styled } from 'styled-components';
+import styled, { keyframes, css } from 'styled-components';
+import { useState, useEffect } from 'react';
 
 const TalkBubble = ({ message }) => {
+  const [visible, setVisible] = useState(true);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setVisible(false);
+    }, 3000); // 3초 후에 사라짐
+
+    // 컴포넌트가 unmount될 때 timer를 정리해주어야 합니다.
+    return () => clearTimeout(timer);
+  }, []); // 빈 dependency 배열을 사용하여 이 effect를 컴포넌트가 mount될 때만 실행하도록 합니다.
+
+  if (!visible) {
+    return null; // 컴포넌트가 보이지 않을 때는 아무것도 렌더링하지 않습니다.
+  }
+
   return (
-    <Container>
+    <Container visible={visible}>
       <TalkPlace>{message}</TalkPlace>
     </Container>
   );
 };
+
+//점점 사라지는 효과
+// const fadeOut = keyframes`
+//   from {
+//     opacity: 1;
+//   }
+//   to {
+//     opacity: 0;
+//   }
+// `;
 
 const Container = styled.div`
   display: flex;
@@ -18,6 +43,14 @@ const Container = styled.div`
   width: 190px;
   height: 160px;
 `;
+// 점점 사라지는 효과 필요하면 Container에 추가
+// opacity: 1;
+// ${(props) =>
+// props.visible
+// ? css`
+//   animation: ${fadeOut} 2s linear;
+//  `
+// : ''}
 
 const TalkPlace = styled.div`
   width: 72%;
