226b67df62fa08d851aeb488bf44220728d5d713
jjjjjeongwon <jjjjjwon21@gmail.com>
2023-07-27 06:00:10 +0900

    :art: Feat: 3D 객체 로딩 완료

---
+++ src/components/ui/Three/3Dcanvas/House.js
@@ -0,0 +1,11 @@
+import { useGLTF } from '@react-three/drei';
+import React from 'react';
+
+const House = () => {
+  const glb = useGLTF('../models/house.glb');
+  const house = glb.scene.children[0];
+  house.position.set(5, 1, 2);
+  return <primitive object={house} dispose={null} />;
+};
+
+export default House;
---
+++ src/components/ui/Three/3Dcanvas/Tree.js
@@ -0,0 +1,11 @@
+import { useGLTF } from '@react-three/drei';
+import React from 'react';
+
+const Tree = () => {
+  const glb = useGLTF('../models/palm_tree.glb');
+  const tree = glb.scene.children[0];
+  tree.position.set(1, 0, 2);
+  return <primitive object={tree} dispose={null} />;
+};
+
+export default Tree;
