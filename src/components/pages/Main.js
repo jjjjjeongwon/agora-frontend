c3a885bc80f7309c1e21b8272725afdb5f4aea80
jjjjjeongwon <jjjjjwon21@gmail.com>
2023-08-02 08:43:31 +0900

    :art: Design: 메인페이지 버튼 스타일 수정

--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -13,14 +13,17 @@ const Main = () => {
   return (
     <Container>
       <LogoContainer>
-        <LoginSignupTitle>PoLY WoRLD </LoginSignupTitle>
-        <AccessButton
-          onClick={() => {
-            navigate('/login');
-          }}
-        >
-          클릭해서 접속하기
-        </AccessButton>
+        <LoginSignupTitle>PoLY WoRD </LoginSignupTitle>
+        <Location>
+          <ButtonBackground />
+          <AccessButton
+            onClick={() => {
+              navigate('/login');
+            }}
+          >
+            START
+          </AccessButton>
+        </Location>
       </LogoContainer>
 
       <HomeICon>
@@ -42,24 +45,39 @@ const LogoContainer = styled.div`
   padding-top: 60px;
 `;
 
-const AccessButton = styled.div`
-  margin: 100px 40px;
+const Location = styled.div`
+  position: absolute;
+`;
+
+const ButtonBackground = styled.div`
+  position: absolute;
   width: 200px;
-  height: 55px;
-  border-radius: 27px;
+  height: 60px;
+  border-radius: 18px;
+  top: 0;
+
+  left: 0;
+  box-shadow: 0 5px #0158b4;
+`;
+
+const AccessButton = styled.div`
+  cursor: pointer;
+  font-family: 'luckiest guy';
+  top: 5px;
   display: flex;
   align-items: center;
   justify-content: center;
-  color: #004ce4;
-  /* font-weight: bold; */
-  font-size: 18px;
-  background-color: transparent;
-  border: 1px solid #004ce4;
-  cursor: pointer;
-  &:hover {
-    transition-duration: 0.3s;
-    background-color: #e8eeff;
-  }
+  font-size: 25px;
+  position: relative;
+  width: 200px;
+  height: 55px;
+  color: white;
+  box-sizing: border-box;
+  padding: 16px 32px;
+  border-radius: 18px;
+  box-shadow: inset 0 -2px 10px #78cfff99;
+  transition: transform 0.2s cubic-bezier(0, 0.51, 0.58, 0.99);
+  background: linear-gradient(180deg, #07b9fe 0%, #1160be 100%);
 `;
 
 const HomeICon = styled.div`
