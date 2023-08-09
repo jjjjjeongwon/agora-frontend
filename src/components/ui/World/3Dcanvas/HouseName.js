0ba8439721d9da16bc3f39ed29833da32eea1167
jjjjjeongwon <jjjjjwon21@gmail.com>
2023-08-10 07:52:03 +0900

    :white_check_mark: Feat: 친구 추가 시 포탈 , 이름 활성화 기능 추가

--- src/components/ui/World/3Dcanvas/HouseName.js
+++ src/components/ui/World/3Dcanvas/HouseName.js
@@ -1,167 +1,244 @@
+// import React, { useEffect, useRef } from 'react';
+// import { useFrame } from '@react-three/fiber';
+// import * as THREE from 'three';
+// import { Float } from '@react-three/drei';
+
+// const HouseName = ({friendsInfo}) => {
+//   const meshRef = useRef();
+
+//   const texCanvas = document.createElement('canvas');
+//   const texCanvas2 = document.createElement('canvas');
+//   const texCanvas3 = document.createElement('canvas');
+//   const texCanvas4 = document.createElement('canvas');
+//   const texCanvas5 = document.createElement('canvas');
+//   const texCanvas6 = document.createElement('canvas');
+
+//   const texContext = texCanvas.getContext('2d');
+//   const texContext2 = texCanvas2.getContext('2d');
+//   const texContext3 = texCanvas3.getContext('2d');
+//   const texContext4 = texCanvas4.getContext('2d');
+//   const texContext5 = texCanvas5.getContext('2d');
+//   const texContext6 = texCanvas6.getContext('2d');
+
+//   texCanvas.width = 500;
+//   texCanvas.height = 500;
+//   texCanvas2.width = 500;
+//   texCanvas2.height = 500;
+//   texCanvas3.width = 500;
+//   texCanvas3.height = 500;
+//   texCanvas4.width = 500;
+//   texCanvas4.height = 500;
+//   texCanvas5.width = 500;
+//   texCanvas5.height = 500;
+//   texCanvas6.width = 500;
+//   texCanvas6.height = 500;
+
+//   const canvasTexture = new THREE.CanvasTexture(texCanvas);
+//   const canvasTexture2 = new THREE.CanvasTexture(texCanvas2);
+//   const canvasTexture3 = new THREE.CanvasTexture(texCanvas3);
+//   const canvasTexture4 = new THREE.CanvasTexture(texCanvas4);
+//   const canvasTexture5 = new THREE.CanvasTexture(texCanvas5);
+//   const canvasTexture6 = new THREE.CanvasTexture(texCanvas6);
+
+//   const geometry = new THREE.PlaneGeometry(5, 5);
+//   const material = new THREE.MeshBasicMaterial({
+//     map: canvasTexture,
+//     transparent: true,
+//     opacity: 1,
+//     side: THREE.DoubleSide,
+//   });
+//   const material2 = new THREE.MeshBasicMaterial({
+//     map: canvasTexture2,
+//     transparent: true,
+//     opacity: 1,
+//     side: THREE.DoubleSide,
+//   });
+//   const material3 = new THREE.MeshBasicMaterial({
+//     map: canvasTexture3,
+//     transparent: true,
+//     opacity: 1,
+//     side: THREE.DoubleSide,
+//   });
+//   const material4 = new THREE.MeshBasicMaterial({
+//     map: canvasTexture4,
+//     transparent: true,
+//     opacity: 1,
+//     side: THREE.DoubleSide,
+//   });
+//   const material5 = new THREE.MeshBasicMaterial({
+//     map: canvasTexture5,
+//     transparent: true,
+//     opacity: 1,
+//     side: THREE.DoubleSide,
+//   });
+//   const material6 = new THREE.MeshBasicMaterial({
+//     map: canvasTexture6,
+//     transparent: true,
+//     opacity: 1,
+//     side: THREE.DoubleSide,
+//   });
+
+//   useFrame(() => {
+//     // Draw on the canvas
+//     texContext.clearRect(0, 0, texCanvas.width, texCanvas.height);
+//     texContext.fillStyle = 'white';
+//     texContext.font = '30px luckiest guy';
+//     texContext.fillText('UUJEEN', 0, 200);
+//     texContext2.clearRect(0, 0, texCanvas.width, texCanvas2.height);
+//     texContext2.fillStyle = 'white';
+//     texContext2.font = '30px luckiest guy';
+//     texContext2.fillText('닉네임', 0, 200);
+//     texContext3.clearRect(0, 0, texCanvas.width, texCanvas3.height);
+//     texContext3.fillStyle = 'white';
+//     texContext3.font = '30px luckiest guy';
+//     texContext3.fillText('성환', 0, 200);
+//     texContext4.clearRect(0, 0, texCanvas.width, texCanvas4.height);
+//     texContext4.fillStyle = 'white';
+//     texContext4.font = '30px luckiest guy';
+//     texContext4.fillText('상주', 0, 200);
+//     texContext5.clearRect(0, 0, texCanvas.width, texCanvas5.height);
+//     texContext5.fillStyle = 'white';
+//     texContext5.font = '30px luckiest guy';
+//     texContext5.fillText('정원', 0, 200);
+//     texContext6.clearRect(0, 0, texCanvas.width, texCanvas6.height);
+//     texContext6.fillStyle = 'white';
+//     texContext6.font = '30px luckiest guy';
+//     texContext6.fillText('파도', 0, 200);
+//   }, []);
+//   return (
+//     <>
+//       <Float
+//         speed={10}
+//         rotationIntensity={0.1}
+//         floatIntensity={0.01}
+//         floatingRange={[0, 0.1]}
+//       >
+//         <mesh
+//           castShadow={false}
+//           ref={meshRef}
+//           geometry={geometry}
+//           material={material}
+//           position={[5.2, 3, -7.7]}
+//         />
+//         <mesh
+//           castShadow={false}
+//           ref={meshRef}
+//           geometry={geometry}
+//           material={material2}
+//           rotation={[0, -Math.PI / 4, 0]}
+//           position={[11, 3, 0]}
+//         />
+//         <mesh
+//           castShadow={false}
+//           ref={meshRef}
+//           geometry={geometry}
+//           material={material3}
+//           rotation={[0, Math.PI / 4, 0]}
+//           position={[-8, 3, -4]}
+//         />
+//         <mesh
+//           castShadow={false}
+//           ref={meshRef}
+//           geometry={geometry}
+//           material={material4}
+//           rotation={[0, Math.PI / 5.5, 0]}
+//           position={[-2, 2.5, -6.8]}
+//         />
+//         <mesh
+//           castShadow={false}
+//           ref={meshRef}
+//           geometry={geometry}
+//           material={material5}
+//           rotation={[0, Math.PI / 4, 0]}
+//           position={[-9.7, 2, 5.5]}
+//         />
+//         <mesh
+//           castShadow={false}
+//           ref={meshRef}
+//           geometry={geometry}
+//           material={material6}
+//           rotation={[0, Math.PI / 4, 0]}
+//           position={[-15, 3, 15]}
+//         />
+//       </Float>
+//     </>
+//   );
+// };
+
+// export default HouseName;
+
 import React, { useEffect, useRef } from 'react';
 import { useFrame } from '@react-three/fiber';
 import * as THREE from 'three';
 import { Float } from '@react-three/drei';
 
-const HouseName = () => {
+const HouseName = ({ friendsInfo }) => {
   const meshRef = useRef();
-  const textRef = useRef();
-  const texCanvas = document.createElement('canvas');
-  const texCanvas2 = document.createElement('canvas');
-  const texCanvas3 = document.createElement('canvas');
-  const texCanvas4 = document.createElement('canvas');
-  const texCanvas5 = document.createElement('canvas');
-  const texCanvas6 = document.createElement('canvas');
+  const positions = [
+    [-15, 3, 15],
+    [-9.7, 2, 5.5],
+    [-2, 2.5, -6.8],
+    [-8, 3, -4],
+    [11, 3, 0],
+    [5.2, 3, -7.7], //내집
+  ];
 
-  const texContext = texCanvas.getContext('2d');
-  const texContext2 = texCanvas2.getContext('2d');
-  const texContext3 = texCanvas3.getContext('2d');
-  const texContext4 = texCanvas4.getContext('2d');
-  const texContext5 = texCanvas5.getContext('2d');
-  const texContext6 = texCanvas6.getContext('2d');
+  const rotations = [
+    [0, 0, 0],
+    [0, -Math.PI / 4, 0],
+    [0, Math.PI / 4, 0],
+    [0, Math.PI / 5.5, 0],
+    [0, Math.PI / 4, 0],
+    [0, Math.PI / 4, 0],
+  ];
 
-  texCanvas.width = 500;
-  texCanvas.height = 500;
-  texCanvas2.width = 500;
-  texCanvas2.height = 500;
-  texCanvas3.width = 500;
-  texCanvas3.height = 500;
-  texCanvas4.width = 500;
-  texCanvas4.height = 500;
-  texCanvas5.width = 500;
-  texCanvas5.height = 500;
-  texCanvas6.width = 500;
-  texCanvas6.height = 500;
-
-  const canvasTexture = new THREE.CanvasTexture(texCanvas);
-  const canvasTexture2 = new THREE.CanvasTexture(texCanvas2);
-  const canvasTexture3 = new THREE.CanvasTexture(texCanvas3);
-  const canvasTexture4 = new THREE.CanvasTexture(texCanvas4);
-  const canvasTexture5 = new THREE.CanvasTexture(texCanvas5);
-  const canvasTexture6 = new THREE.CanvasTexture(texCanvas6);
-
-  const geometry = new THREE.PlaneGeometry(5, 5);
-  const material = new THREE.MeshBasicMaterial({
-    map: canvasTexture,
-    transparent: true,
-    opacity: 1,
-    side: THREE.DoubleSide,
-  });
-  const material2 = new THREE.MeshBasicMaterial({
-    map: canvasTexture2,
-    transparent: true,
-    opacity: 1,
-    side: THREE.DoubleSide,
-  });
-  const material3 = new THREE.MeshBasicMaterial({
-    map: canvasTexture3,
-    transparent: true,
-    opacity: 1,
-    side: THREE.DoubleSide,
-  });
-  const material4 = new THREE.MeshBasicMaterial({
-    map: canvasTexture4,
-    transparent: true,
-    opacity: 1,
-    side: THREE.DoubleSide,
-  });
-  const material5 = new THREE.MeshBasicMaterial({
-    map: canvasTexture5,
-    transparent: true,
-    opacity: 1,
-    side: THREE.DoubleSide,
-  });
-  const material6 = new THREE.MeshBasicMaterial({
-    map: canvasTexture6,
-    transparent: true,
-    opacity: 1,
-    side: THREE.DoubleSide,
+  const canvasRefs = friendsInfo.map(() => {
+    const canvas = document.createElement('canvas');
+    canvas.width = 500;
+    canvas.height = 500;
+    return canvas;
   });
 
   useFrame(() => {
-    // Draw on the canvas
-    texContext.clearRect(0, 0, texCanvas.width, texCanvas.height);
-    texContext.fillStyle = 'white';
-    texContext.font = '30px luckiest guy';
-    texContext.fillText('UUJEEN', 0, 200);
-    texContext2.clearRect(0, 0, texCanvas.width, texCanvas2.height);
-    texContext2.fillStyle = 'white';
-    texContext2.font = '30px luckiest guy';
-    texContext2.fillText('닉네임', 0, 200);
-    texContext3.clearRect(0, 0, texCanvas.width, texCanvas3.height);
-    texContext3.fillStyle = 'white';
-    texContext3.font = '30px luckiest guy';
-    texContext3.fillText('성환', 0, 200);
-    texContext4.clearRect(0, 0, texCanvas.width, texCanvas4.height);
-    texContext4.fillStyle = 'white';
-    texContext4.font = '30px luckiest guy';
-    texContext4.fillText('상주', 0, 200);
-    texContext5.clearRect(0, 0, texCanvas.width, texCanvas5.height);
-    texContext5.fillStyle = 'white';
-    texContext5.font = '30px luckiest guy';
-    texContext5.fillText('정원', 0, 200);
-    texContext6.clearRect(0, 0, texCanvas.width, texCanvas6.height);
-    texContext6.fillStyle = 'white';
-    texContext6.font = '30px luckiest guy';
-    texContext6.fillText('파도', 0, 200);
-  }, []);
+    canvasRefs.forEach((canvas, index) => {
+      const ctx = canvas.getContext('2d');
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.fillStyle = 'white';
+      ctx.font = '30px luckiest guy';
+      ctx.fillText(friendsInfo[index]?.nickname || '', 0, 200);
+    });
+  });
+
+  const textures = canvasRefs.map((canvas) => new THREE.CanvasTexture(canvas));
+  const materials = textures.map((texture) => {
+    return new THREE.MeshBasicMaterial({
+      map: texture,
+      transparent: true,
+      opacity: 1,
+      side: THREE.DoubleSide,
+    });
+  });
+
+  const geometry = new THREE.PlaneGeometry(5, 5);
+
   return (
-    <>
-      <Float
-        speed={10}
-        rotationIntensity={0.1}
-        floatIntensity={0.01}
-        floatingRange={[0, 0.1]}
-      >
+    <Float
+      speed={10}
+      rotationIntensity={0.1}
+      floatIntensity={0.01}
+      floatingRange={[0, 0.1]}
+    >
+      {materials.map((material, index) => (
         <mesh
+          key={index}
           castShadow={false}
-          ref={meshRef}
           geometry={geometry}
           material={material}
-          position={[5.2, 3, -7.7]}
+          position={positions[index]}
+          rotation={rotations[index]}
         />
-        <mesh
-          castShadow={false}
-          ref={meshRef}
-          geometry={geometry}
-          material={material2}
-          rotation={[0, -Math.PI / 4, 0]}
-          position={[11, 3, 0]}
-        />
-        <mesh
-          castShadow={false}
-          ref={meshRef}
-          geometry={geometry}
-          material={material3}
-          rotation={[0, Math.PI / 4, 0]}
-          position={[-8, 3, -4]}
-        />
-        <mesh
-          castShadow={false}
-          ref={meshRef}
-          geometry={geometry}
-          material={material4}
-          rotation={[0, Math.PI / 5.5, 0]}
-          position={[-2, 2.5, -6.8]}
-        />
-        <mesh
-          castShadow={false}
-          ref={meshRef}
-          geometry={geometry}
-          material={material5}
-          rotation={[0, Math.PI / 4, 0]}
-          position={[-9.7, 2, 5.5]}
-        />
-        <mesh
-          castShadow={false}
-          ref={meshRef}
-          geometry={geometry}
-          material={material6}
-          rotation={[0, Math.PI / 4, 0]}
-          position={[-15, 3, 15]}
-        />
-      </Float>
-    </>
+      ))}
+    </Float>
   );
 };
 
--- src/components/ui/World/3Dcanvas/Portal.js
+++ src/components/ui/World/3Dcanvas/Portal.js
@@ -1,6 +1,7 @@
 import { Sparkles } from '@react-three/drei';
+import React from 'react';
 
-const Portal = () => {
+const Portal = ({ friendsInfo }) => {
   const count = 100;
   const noise = 1;
   const baseOpacity = 1;
@@ -10,88 +11,17 @@ const Portal = () => {
   const speed = 10;
   const baseColor = '#00FFFF';
   const pointColor = 'white';
+
+  const positions = [
+    [-17, 0.7, 15],
+    [-11.7, 0.7, 5.5],
+    [-4.2, 0.7, -6.8],
+    [-10, 0.7, -4],
+    [10, 0.7, -2.7],
+  ];
   return (
     <>
-      <Sparkles
-        color={baseColor}
-        position={[-17, 0.7, 15]}
-        count={count}
-        noise={noise}
-        opacity={baseOpacity}
-        size={size}
-        scale={scale}
-        speed={speed}
-      />
-      <Sparkles
-        color={pointColor}
-        position={[-17, 0.7, 15]}
-        count={count}
-        noise={noise}
-        opacity={pointOpacity}
-        size={size}
-        scale={scale}
-        speed={speed}
-      />
-      <Sparkles
-        color={baseColor}
-        position={[-11.7, 0.7, 5.5]}
-        count={count}
-        noise={noise}
-        opacity={baseOpacity}
-        size={size}
-        scale={scale}
-        speed={speed}
-      />
-      <Sparkles
-        color={pointColor}
-        position={[-11.7, 0.7, 5.5]}
-        count={count}
-        noise={noise}
-        opacity={pointOpacity}
-        size={size}
-        scale={scale}
-        speed={speed}
-      />
-      <Sparkles
-        color={baseColor}
-        position={[-4.2, 0.7, -6.8]}
-        count={count}
-        noise={noise}
-        opacity={baseOpacity}
-        size={size}
-        scale={scale}
-        speed={speed}
-      />
-      <Sparkles
-        color={pointColor}
-        position={[-4.2, 0.7, -6.8]}
-        count={count}
-        noise={noise}
-        opacity={pointOpacity}
-        size={size}
-        scale={scale}
-        speed={speed}
-      />
-      <Sparkles
-        color={baseColor}
-        position={[-10, 0.7, -4]}
-        count={count}
-        noise={noise}
-        opacity={baseOpacity}
-        size={size}
-        scale={scale}
-        speed={speed}
-      />
-      <Sparkles
-        color={pointColor}
-        position={[-10, 0.7, -4]}
-        count={count}
-        noise={noise}
-        opacity={pointOpacity}
-        size={size}
-        scale={scale}
-        speed={speed}
-      />
+      {/* 내 집 */}
       <Sparkles
         color={baseColor}
         position={[3.2, 0.7, -7.7]}
@@ -112,26 +42,7 @@ const Portal = () => {
         scale={scale}
         speed={speed}
       />
-      <Sparkles
-        color={baseColor}
-        position={[10, 0.7, -2.7]}
-        count={count}
-        noise={noise}
-        opacity={baseOpacity}
-        size={size}
-        scale={scale}
-        speed={speed}
-      />
-      <Sparkles
-        color={pointColor}
-        position={[10, 0.7, -2.7]}
-        count={count}
-        noise={noise}
-        opacity={pointOpacity}
-        size={size}
-        scale={scale}
-        speed={speed}
-      />
+      {/* 파도타기  */}
       <Sparkles
         color={baseColor}
         position={[35, 0.7, 4]}
@@ -142,6 +53,7 @@ const Portal = () => {
         scale={scale}
         speed={speed}
       />
+
       <Sparkles
         color={pointColor}
         position={[35, 0.7, 4]}
@@ -152,6 +64,33 @@ const Portal = () => {
         scale={scale}
         speed={speed}
       />
+
+      {/* 친구 집 들 */}
+      {friendsInfo.map((friendPosition, idx) => (
+        // 각 친구의 위치에 대해 두 개의 Sparkles 컴포넌트 렌더링
+        <React.Fragment key={idx}>
+          <Sparkles
+            color={baseColor}
+            position={positions[idx]}
+            count={count}
+            noise={noise}
+            opacity={baseOpacity}
+            size={size}
+            scale={scale}
+            speed={speed}
+          />
+          <Sparkles
+            color={pointColor}
+            position={positions[idx]}
+            count={count}
+            noise={noise}
+            opacity={pointOpacity}
+            size={size}
+            scale={scale}
+            speed={speed}
+          />
+        </React.Fragment>
+      ))}
     </>
   );
 };
