6ff06ab118494bdf03cf93ba80d307a1d40dd419
jjjjjeongwon <jjjjjwon21@gmail.com>
2023-08-10 11:56:31 +0900

    :wrench: Fix: 서버주소 변경

--- src/apis/userAPI.js
+++ src/apis/userAPI.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const userAPI = axios.create({
-  baseURL: 'http://3.34.180.83:8080/',
+  baseURL: 'http://3.38.181.151:8080/',
   headers: { 'Content-Type': 'application/json' },
 });
 
